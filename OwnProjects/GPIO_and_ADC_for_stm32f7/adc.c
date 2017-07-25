98a3554492f27884b2cd5fa05d6c190cbaba21ae
marcidelux <marton.lorinczi@gmail.com>
2017-07-26 00:48:05 +0200

    fixed adc.c

--- OwnProjects/GPIO_and_ADC_for_stm32f7/adc.c
+++ OwnProjects/GPIO_and_ADC_for_stm32f7/adc.c
@@ -2,26 +2,19 @@
 
 ADC_HandleTypeDef AdcHandle;
 
- void adc_init()
+ void adc_init(uint8_t pin_index, uint32_t resolution)
 {
-	// A0 is PA0, ADC3 IN0
-
 	// Enable clocks
 	__HAL_RCC_ADC3_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
 
-	// Initialize GPIO as input
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_InitStruct.Pin = GPIO_PIN_0;	// pin 0 of GPIOA
-	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	 //Init analoge pin
+	 gpio_init_analoge_pin(pin_index);
 
 	/*##-1- Configure the ADC peripheral #######################################*/
 	AdcHandle.Instance          		 = ADC3;
 
 	AdcHandle.Init.ClockPrescaler        = ADC_CLOCKPRESCALER_PCLK_DIV4;
-	AdcHandle.Init.Resolution            = ADC_RESOLUTION_12B;
+	AdcHandle.Init.Resolution            = resolution;
 	AdcHandle.Init.ScanConvMode          = DISABLE;
 	AdcHandle.Init.ContinuousConvMode    = DISABLE;
 	AdcHandle.Init.DiscontinuousConvMode = DISABLE;
@@ -36,7 +29,7 @@ ADC_HandleTypeDef AdcHandle;
 	/*##-2- Configure ADC regular channel ######################################*/
 	ADC_ChannelConfTypeDef sConfig;
 
-	sConfig.Channel      = ADC_CHANNEL_0;
+	sConfig.Channel      = stm32f7_analog_pins[pin_index].ADC_channel;
 	sConfig.Rank         = 1;
 	sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
 	sConfig.Offset       = 0;
